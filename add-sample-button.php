0147001f21c1202d4b04b74c6b0387cfd4e2b81b
Kevin Ruscoe <hello@kevinruscoe.me>
2019-04-07 12:48:51 +0100

    clean up

--- add-sample-button.php
+++ add-sample-button.php
@@ -1,13 +1,16 @@
 <?php if (has_maxed_out_sample_allowance()) : ?>
 
-    You max only have <?php print allowed_max_samples() ?> samples.
+    You are only allowed <?php print allowed_max_samples() ?> samples.
 
 <?php else : ?>
 
     <form action="<?php the_permalink() ?>" method="post">
         <?php wp_nonce_field('add_sample_to_cart') ?>
 
-        <input type="hidden" name="add-sample-from-id" value="<?php print wc_get_product()->get_id() ?>">
+        <input 
+            type="hidden" 
+            name="add-sample-from-id" 
+            value="<?php print wc_get_product()->get_id() ?>">
         <button type="submit">Add Sample</button>
     </form>
 
